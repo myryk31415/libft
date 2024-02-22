39de489b0b32c97a8ce6d8a3c1da82128d39c9aa
Myryk <myryk@posteo.de>
2024-02-22 13:05:25 +0100

    fixed strjoin issue on linux

--- src/ft_str/ft_strjoin.c
+++ src/ft_str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:47:29 by padam             #+#    #+#             */
-/*   Updated: 2024/02/19 17:18:43 by padam            ###   ########.fr       */
+/*   Updated: 2024/02/22 13:04:49 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*joint;
 
 	l = ft_strlen(s1) + ft_strlen(s2) + 1;
-	joint = malloc(l * sizeof(char));
+	joint = ft_calloc(l, sizeof(char));
 	if (!joint)
 		return (NULL);
 	if (s1)
