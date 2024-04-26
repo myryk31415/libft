b77c4d0c547a6f96eb645f1ecd3735798330d8fa
Myryk <myryk@posteo.de>
2024-04-26 22:32:59 +0200

    lmao unbroke strtrim

--- src/ft_str/ft_strtrim.c
+++ src/ft_str/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 11:25:28 by padam             #+#    #+#             */
-/*   Updated: 2024/04/26 20:51:19 by padam            ###   ########.fr       */
+/*   Updated: 2024/04/26 22:32:15 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ char	*ft_strtrim(char const *s, char const *set)
 	int		end;
 
 	start = 0;
-	end = ft_strlen(s);
+	end = ft_strlen(s) - 1;
 	if (!s || !set)
 		return ((ft_strdup(s)));
 	while (s[start] && ft_strchr(set, s[start]))
 		start++;
-	while (s[end] && ft_strchr(set, s[end]))
+	while (end && ft_strchr(set, s[end]))
 		end--;
 	return (ft_substr((s), start, end - start + 1));
 }
